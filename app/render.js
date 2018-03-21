304d3ac59672ebea740ee3c260bb5a72238fa623
Shiz <hi@shiz.me>
2018-03-21 04:32:08 +0100

    Move jQuery import to render.js and remove event handler when window closes.

--- app/render.js
+++ app/render.js
@@ -1,23 +1,42 @@
 'use strict';
-const remote = require('electron').remote;
-const nene = remote.getGlobal('nene');
+const $ = require('jquery');
+const nene = require('electron').remote.getGlobal('nene');
+
+
+const setWatching = (source, title, ep) => {
+        const text = title + (ep ? (' - Episode ' + ep) : '');
+        $('title').text(`nene - [${text}]`);
+        $('#watching').text(text);
+        $(`#watching-${source}`).text(text);
+};
+
+const clearWatching = (source, error, wasMain) => {
+        if (wasMain) {
+                $('title').text('nene');
+                $('#watching').text('');
+        }
+        $(`#watching-${source}`).text('');
+};
+
+const setActive = source => {
+        $(`#active-${source}`).text('active');
+};
+
+const clearActive = source => {
+        $(`#active-${source}`).text('');
+};
+
 
 $(() => {
-        nene.on('started', (source, title, ep) => {
-                const text = title + (ep ? (' - Episode ' + ep) : '');
-                $('#watching').text(text);
-                $(`#watching-${source}`).text(text);
-        });
-        nene.on('stopped', (source, error, wasMain) => {
-                if (wasMain) {
-                        $('#watching').text('');
-                }
-                $(`#watching-${source}`).text('');
-        });
-        nene.on('player-active', source => {
-                $(`#active-${source}`).text('active');
-        });
-        nene.on('player-inactive', source => {
-                $(`#active-${source}`).text('');
-        });
+        nene.on('started', setWatching);
+        nene.on('stopped', clearWatching);
+        nene.on('player-active', setActive);
+        nene.on('player-inactive', clearActive);
+});
+
+$(window).on('beforeunload', () => {
+        nene.removeListener('started', setWatching);
+        nene.removeListener('stopped', clearWatching);
+        nene.removeListener('player-active', setActive);
+        nene.removeListener('player-inactive', clearActive);
 });
